e5d37115528c1d687770e30cb7ef05ca50642b11
Ritvik Pande <67699647+ritvikpande@users.noreply.github.com>
2020-07-06 00:54:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+(function(){
+    'use-strict';
+    angular.module('LunchCheck', [])
+    .controller('LunchCheckController', lunchChecker)
+
+    lunchChecker.$inject = ['$scope'];
+
+    function lunchChecker($scope){
+       
+        $scope.lunch = function(){
+
+            $scope.text == " ";
+            
+            var textInput = $scope.textInput;
+            console.log(textInput);
+
+            if(textInput === " " || textInput == null){
+    
+                $scope.text = "Please enter some data";
+            }
+    
+            else{
+                
+                const tot_items = textInput.split(',');
+                //console.log(tot_items);
+
+                var item_length = tot_items.length;
+                console.log('Length is ' +item_length);
+
+                if(item_length > 3){
+    
+                    $scope.text = "Too Much!";
+                }
+                else if(item_length <= 3){
+        
+                    $scope.text = "Enjoy!";
+                }
+            }
+        };
+       
+
+    };
+}
+)();
